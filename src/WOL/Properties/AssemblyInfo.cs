dfc1dd935e0e6545689080af48c1306d1a460f1d
nikeee13 <nikeee@outlook.com>
2013-12-08 02:11:04 +0100

    Add nuspec + builds

--- src/WOL/Properties/AssemblyInfo.cs
+++ src/WOL/Properties/AssemblyInfo.cs
@@ -3,9 +3,6 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System;
 
-// Allgemeine Informationen über eine Assembly werden über die folgenden 
-// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
-// die mit einer Assembly verknüpft sind.
 [assembly: AssemblyTitle("WakeOnLan")]
 [assembly: AssemblyDescription("Stellt Methoden für das Senden von Wake-On-LAN-Signalen bereit.")]
 [assembly: AssemblyConfiguration("")]
@@ -15,23 +12,10 @@ using System;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
-// für COM-Komponenten. Wenn Sie auf einen Typ in dieser Assembly von 
-// COM zugreifen müssen, legen Sie das ComVisible-Attribut für diesen Typ auf "true" fest.
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(true)]
-// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
 [assembly: Guid("4e7980d5-1b85-4f27-a655-aa3a06b5c673")]
 
-// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
-//
-//      Hauptversion
-//      Nebenversion 
-//      Buildnummer
-//      Revision
-//
-// Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
-// übernehmen, indem Sie "*" eingeben:
 [assembly: AssemblyVersion("1.6.*")]
 [assembly: AssemblyFileVersion("1.6.0.0")]
 [assembly: InternalsVisibleTo("WakeOnLan.Testing")]
