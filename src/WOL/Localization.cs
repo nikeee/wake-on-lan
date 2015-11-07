aaf6e37bf9ea516ee31eae1ad997d5179b96798e
Niklas Mollenhauer <nikeee@outlook.com>
2015-11-07 17:44:31 +0100

    Remove German constants

--- src/WOL/Localization.cs
+++ src/WOL/Localization.cs
@@ -4,16 +4,9 @@ namespace System.Net
 {
     internal static class Localization
     {
-#if GERMAN
-        public const string ArgumentExceptionInvalidMacAddressLength = "Die MAC-Adresse hat eine ungültige Länge.";
-        public const string ArgumentExceptionInvalidPasswordLength = "Das Passwort hat eine ungültige Länge.";
-
-        public const string FormatExceptionIsNotAValidMadAddress = " ist keine gültige MAC-Adresse.";
-#else
         public const string ArgumentExceptionInvalidMacAddressLength = "Invalid MAC address length.";
         public const string ArgumentExceptionInvalidPasswordLength = "Invalid password length.";
 
         public const string ArgumentExceptionIsNotAValidMadAddress = " is not a valid MAC address.";
-#endif
     }
 }
