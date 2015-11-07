57043748f74038aa4fe0b9c6ec052fa5e1bfd57d
Niklas Mollenhauer <nikeee@outlook.com>
2015-11-07 17:43:36 +0100

    Remove previously obsolete MacAddress class

--- src/WOL/Localization.cs
+++ src/WOL/Localization.cs
@@ -5,15 +5,11 @@ namespace System.Net
     internal static class Localization
     {
 #if GERMAN
-        public const string ObsoleteMacAddress = "Die MacAddress-Klasse ist veraltet. Verwenden Sie die Klasse System.Net.NetworkInformation.PhysicalAddress.";
-
         public const string ArgumentExceptionInvalidMacAddressLength = "Die MAC-Adresse hat eine ungültige Länge.";
         public const string ArgumentExceptionInvalidPasswordLength = "Das Passwort hat eine ungültige Länge.";
 
         public const string FormatExceptionIsNotAValidMadAddress = " ist keine gültige MAC-Adresse.";
 #else
-        public const string ObsoleteMacAddress = "The MacAddress class is obsolete. Consider using the System.Net.NetworkInformation.PhysicalAddress class.";
-
         public const string ArgumentExceptionInvalidMacAddressLength = "Invalid MAC address length.";
         public const string ArgumentExceptionInvalidPasswordLength = "Invalid password length.";
 
