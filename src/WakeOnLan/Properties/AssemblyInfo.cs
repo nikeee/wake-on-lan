f4e22f7c535922b61f2a107715ee568c4f20919d
Niklas Mollenhauer <nikeee@outlook.com>
2018-12-02 03:58:10 +0100

    Remove some assembly info

--- src/WakeOnLan/Properties/AssemblyInfo.cs
+++ src/WakeOnLan/Properties/AssemblyInfo.cs
@@ -3,19 +3,4 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System;
 
-[assembly: AssemblyTitle("WakeOnLan")]
-[assembly: AssemblyDescription("Stellt Methoden für das Senden von Wake-On-LAN-Signalen bereit.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WakeOnLan")]
-[assembly: AssemblyCopyright("Copyright © Niklas Mollenhauer 2012")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-[assembly: ComVisible(false)]
-[assembly: CLSCompliant(true)]
-[assembly: Guid("4e7980d5-1b85-4f27-a655-aa3a06b5c673")]
-
-[assembly: AssemblyVersion("1.6.*")]
-[assembly: AssemblyFileVersion("1.6.0.0")]
 [assembly: InternalsVisibleTo("WakeOnLan.Tests")]
